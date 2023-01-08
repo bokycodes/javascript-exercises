ed492eca71d6a37b236a8c352c6b7dd07922adfc
bokycodes <114671954+bokycodes@users.noreply.github.com>
2023-01-08 20:49:54 +0100

    finished calc exersises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(...array) {
+	let sum = 0;
+  for(let i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(niz) {
+  let sum = 0;
+  for(let i = 0; i < niz.length; i++){
+    sum = sum + niz[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let res = 1;
+  for(let i = 0; i < array.length; i++){
+    res = res * array[i];
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  // 4 and 3 = 4 * 4 * 4
+  let c = a;
+	for(let i = 1; i < b; i++){
+    a = a*c;
+  }
+  return a;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	// 4 = 4*3*2*1
+  let factorial = 1;
+  if(a === 0 || a === 1){
+    return 1;
+  }
+  for(let i = a; i > 0; i--){
+    factorial = factorial * i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
