fcb0c31a69994691ddcff8fc78d6dd41b9e6b734
bokycodes <114671954+bokycodes@users.noreply.github.com>
2023-01-08 21:14:13 +0100

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let str1 = str.replace(/[^a-zA-Z0-9 ]/g, ''); //removes all special characters
+    str1 = str1.toLowerCase();
+    str1 = str1.replace(/\s/g, ""); //removes all white spaces -> trim() only removes white spaces from beggining and end of a string, not in the middle
+    let str2 = "";
+    for(let i = str1.length-1; i >= 0; i--){
+        str2 = str2 + str1.charAt(i);
+    }
 
+    if(str1 === str2){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
